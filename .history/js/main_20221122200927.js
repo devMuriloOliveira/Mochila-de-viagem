ae61eb2128b988deb2fd765747c78687163ea995
murilo <muriloleiteoliveira1@gmail.com>
2022-12-06 20:04:51 -0300

    commit

---
+++ .history/js/main_20221122200927.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('novoItem')
+const lista = document.getElementById('lista')
+const itens = JSON.parse(localStorage.getItem('itens')) || []
+
+itens.forEach(elemento => {
+  console.log(elemento.nome, elemento.quantidade)
+})
+
+form.addEventListener('submit', evento => {
+  evento.preventDefault()
+
+  const nome = evento.target.elements['nome']
+  const quantidade = evento.target.elements['quantidade']
+
+  const itemAtual = {
+    nome: nome,
+    quantidade: quantidade
+  }
+
+  criaElemento(nome.value, quantidade.value)
+
+  itens.push(itemAtual)
+
+  localStorage.setItem('itens', JSON.stringify(itens))
+  nome.value = ''
+  quantidade.value = ''
+})
+
+function criaElemento(nome, quantidade) {
+  const novoItem = document.createElement('li')
+  novoItem.classList.add('item')
+
+  const numeroItem = document.createElement('strong')
+  numeroItem.innerHTML = quantidade
+
+  novoItem.appendChild(numeroItem)
+  novoItem.innerHTML += nome
+
+  lista.appendChild(novoItem)
+}
